1f78f6a5a2c32920511fee22a10eeb8e37561eb1
Yuri <yuriqpaiva@gmail.com>
2022-01-20 21:25:01 -0300

    trabalhando com xmlHttpRequest com Promises

---
+++ service/cliente-service.js
@@ -0,0 +1,38 @@
+const criaNovalinha = (nome, email) => {
+  const linhaNovoCliente = document.createElement('tr');
+  const conteudo = `<td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td> `;
+  linhaNovoCliente.innerHTML = conteudo;
+  return linhaNovoCliente;
+};
+
+const tabela = document.querySelector('[data-tabela]');
+
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+
+    http.open('GET', 'http://localhost:3000/profile');
+    http.send();
+    http.onload = () => {
+      if (http.status >= 400) {
+        reject(JSON.parse(http.response));
+      } else {
+        resolve(JSON.parse(http.response));
+      }
+    };
+  });
+  return promise;
+};
+
+listaClientes().then((data) => {
+  data.forEach(({ nome, email }) => {
+    tabela.appendChild(criaNovalinha(nome, email));
+  });
+});
