d5f16f85c48569e4988a2334fe1c05a23255dc5c
Yuri <yuriqpaiva@gmail.com>
2022-01-21 13:46:27 -0300

    código assíncrono e event loop

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,19 +1,3 @@
-const criaNovalinha = (nome, email) => {
-  const linhaNovoCliente = document.createElement('tr');
-  const conteudo = `<td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td> `;
-  linhaNovoCliente.innerHTML = conteudo;
-  return linhaNovoCliente;
-};
-
-const tabela = document.querySelector('[data-tabela]');
-
 const listaClientes = () => {
   // Utilizando Promise com xmlHttpRequest:
   //   const promise = new Promise((resolve, reject) => {
@@ -35,8 +19,4 @@ const listaClientes = () => {
   });
 };
 
-listaClientes().then((data) => {
-  data.forEach(({ nome, email }) => {
-    tabela.appendChild(criaNovalinha(nome, email));
-  });
-});
+export const clienteService = { listaClientes };
